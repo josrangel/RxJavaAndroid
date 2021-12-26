19349f2378056e3c7306262513262c17c9ae464f
josrangel <josrangel@hotmail.com>
2021-12-25 23:11:08 -0600

    - add functions - add source links

--- app/src/main/java/com/josrangel/rxjavaandroid/MainActivity.java
+++ app/src/main/java/com/josrangel/rxjavaandroid/MainActivity.java
@@ -3,12 +3,34 @@ package com.josrangel.rxjavaandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.josrangel.rxjavaandroid.interfaces.MainPresenter;
+import com.josrangel.rxjavaandroid.interfaces.MainView;
+
+/**
+ * Source:
+ * https://www.adictosaltrabajo.com/2015/09/14/introduccion-a-rxjava-en-android/
+ * https://medium.com/gradeup/simplest-rxjava-tutorial-for-android-262013ce3545
+ */
+
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private TextView tvMain;
+    private MainPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMain = findViewById(R.id.tv_main);
+
+        presenter = new MainPresenterImpl(this);
+        presenter.getListAnimals();
+    }
+
+    public void showData(String data) {
+        tvMain.setText(data);
     }
 }
